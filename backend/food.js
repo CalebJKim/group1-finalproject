239d5cafffb49a72f37aa1f0e72a63de5a4a8022
Geoffrey Xiang <geoffreyxiang@Geoffreys-MacBook-Air.local>
2022-04-20 17:39:33 -0700

    updated most requests

--- backend/food.js
+++ backend/food.js
@@ -22,29 +22,88 @@ const foodSchema = Schema({
         type: String, 
         required: true
     },
-    categories: {
+    category: {
         type: String, 
         required: true
-    } // needs work cuz idk how nested schema or json-looking things work
+    }, 
+    foodItem: {
+        type: String, 
+        required: true
+    }, 
+    rating: {
+        type: Number
+    }
 })
 
 const FOOD = mongoose.model('foods', foodSchema);
 
-app.get("/", (req, res) => {
+app.get("/all", (req, res) => {
     // fill in get request for all foods
     // not gonna be used in the app but it seems useful for future testing
+    FOOD.find().then((food) => {
+        res.json(food)
+        // res.json({foods: food})
+    })
+    // res.json({foods: foodSchema})
 })
 
 app.get("/dining-halls", (req, res) => {
     // should return all the dining halls in existence
+    FOOD.find().then((food) => {
+        res.json(food.diningHall)
+    })
+    res.json(foodSchema.diningHall)
 })
 
 app.post("/add", (req, res) => {
+    const food = new FOOD({
+        diningHall: req.body.diningHall,
+        category: req.body.category,
+        foodItem: req.body.foodItem
+    })
+    food.save((error, document) => {
+        if (error) {
+            res.json({ title: 'failure to add' })
+        }
+        else {
+            res.json({  diningHall: req.body.diningHall,
+                category: req.body.category,
+                foodItem: req.body.foodItem
+            })
+        }
+    })
     // should allow for adding of food options to dining halls (must be specified)
-    // tbh idk why we need this, but idk how just making a database will work so this it is :D
 })
 
+app.post("/add/with-rating", (req, res) => {
+    const food = new FOOD({
+        diningHall: req.body.diningHall,
+        category: req.body.category,
+        foodItem: req.body.foodItem, 
+        rating: req.body.rating
+    })
+    food.save((error, document) => {
+        if (error) {
+            res.json({ title: 'failure to add with rating' })
+        }
+        else {
+            res.json({  foodItem: req.body.foodItem, 
+                rating: req.body.rating
+            })
+        }
+    })
+    // should allow for adding of food options to dining halls (must be specified)
+})
 
+app.delete("/remove/all", (req, res) => {
+    // should remove all foods for the day
+    // likely useful when day changes
+    FOOD.deleteMany({ }).then(function(){
+        console.log("Cleared");
+    }).catch(function(error){
+        console.log(error);
+    });
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
