79aae74782fcd0471207a452dd80a53a9d40b9d5
Rachel Xin <rachelxin@berkeley.edu>
2022-04-22 08:32:30 -0700

    Add routes

---
+++ frontend/src/Components/Home.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div>
+            <h1>HOME</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ frontend/src/Components/User.js
@@ -0,0 +1,9 @@
+function User() {
+    return (
+        <div>
+            <h1>SIGN IN</h1>
+        </div>
+    )
+}
+
+export default User;
