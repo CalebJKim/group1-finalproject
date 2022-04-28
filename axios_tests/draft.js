7a7e545ca8b7657a7e44d9d8750f01ea41ac73b1
Geoffrey Xiang <geoffreyxiang@Geoffreys-MacBook-Air.local>
2022-04-27 22:02:53 -0700

    SUCCESSFULLY MADE MENU DATABASE

--- axios_tests/draft.js
+++ axios_tests/draft.js
@@ -4,10 +4,10 @@ const url = 'http://localhost:3000'
 
 // adding data to own API
 function axiosPost(foodJson) {
-    const promise = axios.post(url + '/add/with-rating', foodJson)
+    const promise = axios.post(url + '/add', foodJson)
     const result = promise.then((response) => {
         // console.log(response.config);
-        console.log('successlly posted')
+        // console.log('successfully posted')
       }, (error) => {
         console.log(error);
       })
@@ -37,7 +37,7 @@ async function getMenu(location, time) { // location is an int, time must be 'Br
     let allFood = await getAllFood()
     let menu = JSON.parse(allFood)[location][time]
     timeSpecificMenu = []
-    //console.log(menu)
+    console.log(menu)
     for (var key of Object.keys(menu)) {
         category = [key]
         for (let i = 0; i < menu[key].length; i++) {
@@ -45,26 +45,82 @@ async function getMenu(location, time) { // location is an int, time must be 'Br
         }
         timeSpecificMenu.push(category)
     }
-    console.log(timeSpecificMenu)
+    //console.log(timeSpecificMenu)
     return timeSpecificMenu
 }
 
-getMenu(1, 'Lunch')
-
 function updateLocation(location) {
-    if (location == "Cafe 3") {
+    if (location === "Cafe 3") {
+        return "Clark Kerr Campus";
+    }
+    if (location === "Clark Kerr Campus") {
+        return "Crossroads";
+    }
+    if (location === "Crossroads") {
+        return "Foothill"
+    }
+    if (location === "Foothill") { 
+        return "STOP LOCATION"
+    }
+    return "ERROR - LOCATION NOT FOUND" // shouldn't be reached
+}
 
+function locationNumber(location) {
+    if (location == "Cafe 3") {
+        return 0
+    }
+    if (location === "Clark Kerr Campus") {
+        return 1
+    }
+    if (location === "Crossroads") {
+        return 2
+    }
+    if (location === "Foothill") {
+        return 3
+    }
+    return "ERROR - INVALID LOCATION" // shouldn't be reached
+}
+
+function updateTime(time) {
+    if (time === "Breakfast") {
+        return "Lunch"
+    }
+    if (time === "Lunch") {
+        return "Dinner"
+    }
+    if (time === "Dinner") { 
+        return "STOP TIME"
+    }
+    return "ERROR - TIME NOT FOUND" // shouldn't be reached
+}
+
+function createAndPostJSON(location, time, menu) {
+    for (let i = 0; i < menu.length; i++) {
+        let category = menu[i][0]
+        for (let j = 1; j < menu[0].length; j++) {
+            let tempJSON = {
+                diningHall: location, 
+                category: category, 
+                time: time, 
+                foodItem: menu[i][j]
+            }
+            axiosPost(tempJSON)
+        }
     }
 }
 
 async function updateAPI() {
     var location = "Cafe 3"
     var time = "Breakfast"
-    while (location < 4) {
-        while (timeNumber < 3) {
-            if (timeNumber == 0) {
-                var time = 'Breakfast'
-            }
+    while (location !== "STOP LOCATION") {
+        let number = locationNumber(location)
+        while (time !== "STOP TIME") {
+            let menu = await getMenu(number, time)
+            createAndPostJSON(location, time, menu)
+            time = updateTime(time)
         }
+        location = updateLocation(location)
+        time = "Breakfast"
     }
 }
+updateAPI()
